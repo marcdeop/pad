2eaed775fc9e7de64f7fba5e08c2bcfdac77a861
Marc Deop <marc@marcdeop.com>
2016-11-17 20:59:41 +0100

    Make ChatClient use ClientInputThread and ClientOutputThread

--- chat/ChatClient.java
+++ chat/ChatClient.java
@@ -7,32 +7,9 @@ public class ChatClient {
     final MySocket sc = new MySocket(args[0], Integer.parseInt(args[1]));
 
     // Input thread
-    new Thread() {
-      public void run() {
-      String line;
-      BufferedReader kbd = new BufferedReader(new InputStreamReader(System.in));
-      try {
-        while ((line = kbd.readLine()) != null ) 
-            sc.println(line);
-        sc.shutdownOutput();
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-      }
-    }.start();
+    new ClientInputThread(sc).start();
 
     // Output thread
-    new Thread() {
-      public void run() {
-        String line;
-        try {
-          while ((line = sc.readLine()) != null )
-            System.out.println(line);
-          sc.close();
-        } catch(Exception e){
-          e.printStackTrace();
-        }
-      }
-    }.start();
+    new ClientOutputThread(sc).start();
   }
 }
